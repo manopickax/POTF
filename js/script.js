7a228e75deb6c8cd3fcca754150820ea06cbe880
manopickax <rnrrbs0202@gmail.com>
2023-03-27 16:08:22 +0900

    반응형 수정

--- js/script.js
+++ js/script.js
@@ -9,4 +9,7 @@ let observer = new IntersectionObserver((entries) => {
   });
   
   let div = document.querySelectorAll('div');
+  observer.observe(div[0]);
   observer.observe(div[1]);
+  observer.observe(div[2]);
+  observer.observe(div[3]);
