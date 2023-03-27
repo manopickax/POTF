888156a4c39f72b4831ecdfd7e577acc87f89934
manopickax <rnrrbs0202@gmail.com>
2023-03-27 11:31:45 +0900

    0.01

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  
+  let div = document.querySelectorAll('div');
+  observer.observe(div[1]);
